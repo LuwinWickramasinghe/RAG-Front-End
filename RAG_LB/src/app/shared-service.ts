839c332c7825dcbb0da14cdb9b7e7934e660629a
Luwin Wickramasinghe <luwin.anuka@gmail.com>
2025-03-28 16:18:11 +0530

    service to maintain variables

---
+++ RAG_LB/src/app/shared-service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SharedService {
+
+  constructor() { }
+
+  private showChat = new BehaviorSubject<boolean>(true); // default minimized
+  showChat$ = this.showChat.asObservable();
+
+  private isClosed = new BehaviorSubject<boolean>(true); // default minimized
+  isClosed$ = this.showChat.asObservable();
+
+  setShowChat(state: boolean) {
+    this.showChat.next(state);
+  }
+
+  getCurrentState() {
+    return this.showChat.value;
+  }
+
+  setIsClosed(state: boolean) {
+    this.isClosed.next(state);
+  }
+
+  getIsClosed() {
+    return this.isClosed.value;
+  }
+}
